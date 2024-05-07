e96ae7234e0d065f278acf0a3d27724d4767c64e
César Tzoc <csartalva01@gmail.com>
2024-05-07 02:52:26 -0600

    feat: adding career service

--- src/main/java/com/ayd2/adm/library/system/repository/AdmCareerRepository.java
+++ src/main/java/com/ayd2/adm/library/system/repository/AdmCareerRepository.java
@@ -3,5 +3,11 @@ package com.ayd2.adm.library.system.repository;
 import com.ayd2.adm.library.system.model.AdmCareer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdmCareerRepository extends JpaRepository<AdmCareer, Long> {
+
+    Optional<AdmCareer> findByCode(String code);
+
+    Optional<AdmCareer> findByCodeAndCareerIdNot(String code, Long careerId);
 }
---
+++ src/main/java/com/ayd2/adm/library/system/service/AdmCareerService.java
@@ -0,0 +1,43 @@
+package com.ayd2.adm.library.system.service;
+
+import com.ayd2.adm.library.system.exception.LibException;
+import com.ayd2.adm.library.system.model.AdmCareer;
+import com.ayd2.adm.library.system.repository.AdmCareerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AdmCareerService {
+
+    private final AdmCareerRepository careerRepository;
+
+    public AdmCareerService(AdmCareerRepository careerRepository) {
+        this.careerRepository = careerRepository;
+    }
+
+    public Optional<AdmCareer> findById(Long careerId) {
+        return careerRepository.findById(careerId);
+    }
+
+    public List<AdmCareer> findAll() {
+        return careerRepository.findAll();
+    }
+
+    public AdmCareer create(AdmCareer entity) throws LibException {
+        var careerByCode = careerRepository.findByCode(entity.getCode());
+        if (careerByCode.isPresent()) throw new LibException("code_already_exists");
+        return careerRepository.save(entity);
+    }
+
+    public AdmCareer update(Long careerId, AdmCareer entity) throws LibException {
+        var entityDb = careerRepository.findById(careerId);
+        if (entityDb.isEmpty()) throw new LibException("career_not_found").status(HttpStatus.NOT_FOUND);
+        if (!entityDb.get().getCareerId().equals(entity.getCareerId())) throw new LibException("invalid_update");
+        var careerByCode = careerRepository.findByCodeAndCareerIdNot(entity.getCode(), careerId);
+        if (careerByCode.isPresent()) throw new LibException("code_already_exists");
+        return careerRepository.save(entity);
+    }
+}
