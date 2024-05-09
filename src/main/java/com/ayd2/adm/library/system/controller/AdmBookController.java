cbb8a0e99eaf6f77733bd3ed9f38dbf03ab2106e
César Tzoc <csartalva01@gmail.com>
2024-05-08 20:14:59 -0600

    feat: adding book controller

---
+++ src/main/java/com/ayd2/adm/library/system/controller/AdmBookController.java
@@ -0,0 +1,52 @@
+package com.ayd2.adm.library.system.controller;
+
+import com.ayd2.adm.library.system.exception.LibException;
+import com.ayd2.adm.library.system.model.AdmBook;
+import com.ayd2.adm.library.system.service.AdmBookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("books")
+public class AdmBookController {
+
+    private final AdmBookService bookService;
+
+    public AdmBookController(AdmBookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AdmBook>> findAll() {
+        var books = bookService.findAll();
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<AdmBook> findById(@PathVariable("bookId") Long bookId) {
+        return bookService.findById(bookId)
+                .map(ResponseEntity::ok)
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<AdmBook> create(@RequestBody AdmBook entity) throws LibException {
+         var newEntity = bookService.create(entity);
+         return new ResponseEntity<>(newEntity, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{bookId}")
+    public ResponseEntity<AdmBook> create(@PathVariable("bookId") Long bookId, @RequestBody AdmBook entity) throws LibException {
+        var updatedEntity = bookService.update(bookId, entity);
+        return ResponseEntity.ok(updatedEntity);
+    }
+}
--- src/main/java/com/ayd2/adm/library/system/service/AdmBookService.java
+++ src/main/java/com/ayd2/adm/library/system/service/AdmBookService.java
@@ -1,9 +1,12 @@
 package com.ayd2.adm.library.system.service;
 
+import com.ayd2.adm.library.system.exception.LibException;
 import com.ayd2.adm.library.system.model.AdmBook;
 import com.ayd2.adm.library.system.repository.AdmBookRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +20,26 @@ public class AdmBookService {
 
     public Optional<AdmBook> findById(Long bookId) {
         return bookRepository.findById(bookId);
+    }
+
+    public List<AdmBook> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public AdmBook create(AdmBook entity) throws LibException {
+        var isbn = entity.getIsbn();
+        var bookByIsbn = bookRepository.findByIsbn(isbn);
+        if (bookByIsbn.isPresent()) throw new LibException("isbn_already_exists");
+        return bookRepository.save(entity);
+    }
+
+    public AdmBook update(Long bookId, AdmBook entity) throws LibException {
+        var entityDb = bookRepository.findById(bookId);
+        if (entityDb.isEmpty()) throw new LibException("book_not_found").status(HttpStatus.NOT_FOUND);
+        if (!entityDb.get().getBookId().equals(entity.getBookId())) throw new LibException("invalid_update");
+
+        var bookByIsbn = bookRepository.findByIsbnAndBookIdNot(entity.getIsbn(), bookId);
+        if (bookByIsbn.isEmpty()) throw new LibException("isbn_already_exists");
+        return bookRepository.save(entity);
     }
 }
