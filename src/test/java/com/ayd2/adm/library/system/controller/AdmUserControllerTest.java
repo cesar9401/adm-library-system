8844d8cb6d20e898e452df8f2e822a5f6488d235
César Tzoc <csartalva01@gmail.com>
2024-05-20 09:04:48 -0600

    feat: adding test for user controller

--- src/test/java/com/ayd2/adm/library/system/controller/AdmUserControllerTest.java
+++ src/test/java/com/ayd2/adm/library/system/controller/AdmUserControllerTest.java
@@ -84,6 +84,74 @@ class AdmUserControllerTest {
 
     @Test
     @WithMockUser
+    public void updateUserTest() throws Exception {
+        final var email = "demo.email@cunoc.edu.gt";
+        final var name = "Nombre demo";
+
+        final var newEmail = "luismi@cunoc.edu.gt";
+        final var newName = "Luis Miguel Gallego";
+
+        final var user = new AdmUser();
+        user.setEmail(email);
+        user.setFullName(name);
+        user.setPassword("7BLxqNgnQvjb2b");
+
+        var result = mockMvc.perform(
+                        post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(user))
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userId").isNumber())
+                .andReturn();
+
+        var json = result.getResponse().getContentAsString();
+        var createdUser = mapper.readValue(json, AdmUser.class);
+
+        assertAll(
+                () -> assertNotNull(createdUser),
+                () -> assertNotNull(createdUser.getUserId()),
+                () -> assertEquals(email, user.getEmail()),
+                () -> assertEquals(name, user.getFullName())
+        );
+
+        var userId = createdUser.getUserId();
+        // find by id
+        var resultById = mockMvc.perform(
+                        get("/users/{userId}", userId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var userById = mapper.readValue(resultById.getResponse().getContentAsString(), AdmUser.class);
+        assertAll(
+                () -> assertNotNull(userById),
+                () -> assertEquals(email, userById.getEmail()),
+                () -> assertEquals(userId, userById.getUserId())
+        );
+
+        // update email and name
+        userById.setEmail(newEmail);
+        userById.setFullName(newName);
+
+        // test update
+        mockMvc.perform(
+                        put("/users/{userId}", userId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(userById))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.fullName").value(newName))
+                .andExpect(jsonPath("$.email").value(newEmail))
+                .andExpect(jsonPath("$.userId").value(userId));
+    }
+
+    @Test
+    @WithMockUser
     public void findAllTest() throws Exception {
         final var page = 1;
         final var size = 20;
